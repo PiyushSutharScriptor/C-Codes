0e89723bcaa7d1b183a8ef2eb8f3483563401b0d
Sadeem <diamond.trilogies@gmail.com>
2024-08-12 04:07:01 +0300

    Update 2ReverseOfElements.c
    Made lots of modifications to make the code more durable and enduring. Used comma operator to reduce the reverse function by (1/3) lines of code; used "sizeof(array_name)/sizeof(*array_name)" to make the reverse function work with any variable length of arrays; used size_t (standard alias for "unsigned long" which can hold the largest possible value for your computer's architecture) as the datatype for the array length. Used `?:` conditional operator in printf to nicely print out the array.

--- 2ReverseOfElements.c
+++ 2ReverseOfElements.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
-void reverse(int a[])
-{
-    int i=0 ;
-    int j=4;
-    while(i<j)
-    {
-        int temp = a[i];
-        a[i]=a[j];
-        a[j]=temp;
-        i++;
-        j--;
+
+void reverse(int a[], size_t size) {
+    int temp;
+    for (int i = 0, j = size; i < j; ++i, --j) {
+        temp = a[i], a[i] = a[j], a[j] = temp;
     }
-    return ;
- 
 }
-int main ()
-{
-    
+
+int main () {
     int a[5] = {1,2,3,4,5};
-    reverse(a);
-    for (int i=0 ; i<=4 ; i++)
-    {
-       printf ("%d ",a[i]); 
+    size_t asize = sizeof(a)/sizeof(*a);
+    reverse(a, asize);
+
+    putchar('[');
+    for (int i = 0; i < asize; i++) {
+       printf("%d%s",a[i], (i < asize - 1) ? ", " : "]\n");
     }
    return 0;
 }
